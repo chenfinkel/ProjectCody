7f0d1395602c4c32b26149c2404eaef13a6d4cc0
chenfinkel <chenfi@post.bgu.ac.il>
2018-10-16 21:41:33 +0300

    connecting to db from main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -16,8 +20,33 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Connect to a sample database
+     *
+     * @param fileName the database file name
+     */
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:C:/Users/chenfi/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     public static void main(String[] args) {
+
+
+
+        createNewDatabase("users.db");
         launch(args);
     }
 }
