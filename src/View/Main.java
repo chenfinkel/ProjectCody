51f4726d43acd3a33f313af2eef255b8eab1380d
cordoval <cordoval@post.bgu.ac.il>
2018-12-08 17:01:31 +0200

    searchVac

--- src/View/Main.java
+++ src/View/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class Main extends Application {
 
@@ -37,6 +38,39 @@ public class Main extends Application {
         }
     }
 
+    public static void createVacationTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:Vacation4U.db";
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS vacation (\n"
+                + "id text PRIMARY KEY,\n"
+                + "userName text NOT NULL,\n"
+                + "airline text NOT NULL,\n"
+                + "from text NOT NULL, \n"
+                + "destination text NOT NULL, \n"
+                + "Depart text NOT NULL, \n"
+                + "Return text NOT NULL, \n"
+                + "travelersA text, \n"
+                + "travelersC text, \n"
+                + "travelersB text, \n"
+                + "direct INTEGER, \n"
+                + "price text NOT NULL, \n"
+                + "baggage text, \n"
+                + "type text, \n"
+                + "hotel text, \n"
+                + "hotelRating text, \n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     public static void createUsersTable() {
         // SQLite connection string
         String url = "jdbc:sqlite:Vacation4U.db";
@@ -63,6 +97,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         createNewDatabase();
         createUsersTable();
+        createVacationTable();
         launch(args);
     }
 }
